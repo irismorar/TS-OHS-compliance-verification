c395581dad0a9f5a031fc3930e2ccb40eb7ac0d9
Iris Adina Morar <morariris@gmail.com>
2026-07-17 14:58:04 +0300

    small change of the probability/gravity log

--- src/components/RiskTable/RiskTable.stories.ts
+++ src/components/RiskTable/RiskTable.stories.ts
@@ -17,7 +17,7 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     onCommitRiskValues: (gravity, probability) => {
-      console.log("Committed risk values:", gravity, probability);
+      console.log("Committed risk values:", probability, gravity);
     },
   },
 };
