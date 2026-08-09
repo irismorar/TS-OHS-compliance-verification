09ab8859c865b6b9f454c929fcb2b6cbfd9ea175
Iris Morar <morariris@gmail.com>
2026-08-09 16:25:38 +0300

    Start improving the risk table.

---
+++ src/components/RiskTable/RiskTableCell.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { RiskTableCell } from "./RiskTableCell";
+
+const meta = {
+  title: "Components/RiskTableCell",
+  component: RiskTableCell,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof RiskTableCell>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: { riskLevel: "E", isLockedIn: false, disabled: false },
+};
---
+++ src/components/RiskTable/RiskTableCell.tsx
@@ -0,0 +1,74 @@
+import { cva } from "class-variance-authority";
+
+type Props = {
+  riskLevel: "E" | "M" | "S";
+  gravity: 1 | 2 | 3 | 4;
+  probability: 1 | 2 | 3 | 4;
+  isLockedIn: boolean;
+  disabled: boolean;
+  onHover: (gravity: 1 | 2 | 3 | 4, probability: 1 | 2 | 3 | 4) => void;
+  onLockIn: (gravity: 1 | 2 | 3 | 4, probability: 1 | 2 | 3 | 4) => void;
+  onLeave: () => void;
+};
+
+export function RiskTableCell({
+  riskLevel,
+  gravity,
+  probability,
+  isLockedIn,
+  disabled,
+  onLockIn,
+  onHover,
+  onLeave,
+}: Props) {
+  if (disabled) {
+    return (
+      <td className="w-10 h-10 text-center align-middle bg-slate-400"></td>
+    );
+  }
+
+  return (
+    <td
+      className={riskTableCellVariants({ riskLevel, isLockedIn })}
+      onClick={() => onLockIn(gravity, probability)}
+      onMouseEnter={() => onHover(gravity, probability)}
+      onMouseLeave={onLeave}
+    >
+      {riskLevel}
+    </td>
+  );
+}
+
+const riskTableCellVariants = cva(
+  ["w-10 h-10 text-center align-middle cursor-pointer"],
+  {
+    variants: {
+      riskLevel: {
+        E: ["bg-red-300 hover:bg-red-600"],
+        M: ["bg-amber-200 hover:bg-amber-500"],
+        S: ["bg-green-300 hover:bg-green-600"],
+      },
+      isLockedIn: {
+        false: null,
+        true: ["border-2", "border-white"],
+      },
+    },
+    compoundVariants: [
+      {
+        riskLevel: "E",
+        isLockedIn: true,
+        className: ["bg-red-600"],
+      },
+      {
+        riskLevel: "M",
+        isLockedIn: true,
+        className: ["bg-amber-500"],
+      },
+      {
+        riskLevel: "S",
+        isLockedIn: true,
+        className: ["bg-green-600"],
+      },
+    ],
+  },
+);
